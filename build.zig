f406f94e761f79cd8bd5d0606be13e100e5fe014
significant-cijfer <fake.potsjem@gmail.com>
2026-01-18 22:10:17 +0100

    Changed project name to lego

--- build.zig
+++ build.zig
@@ -4,19 +4,19 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const mod = b.addModule("graph", .{
+    const mod = b.addModule("lego", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
     });
 
     const exe = b.addExecutable(.{
-        .name = "graph",
+        .name = "lego",
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
             .target = target,
             .optimize = optimize,
             .imports = &.{
-                .{ .name = "graph", .module = mod },
+                .{ .name = "lego", .module = mod },
             },
         }),
     });
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
-# Marco - Graph
+# Lego
+
+Simple codegen library written for [duplo](https://github.com/significant-cijfer/duplo).
